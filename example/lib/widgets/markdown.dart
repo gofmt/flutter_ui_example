080f67c3f2daaf0f124aa3b6ce3954d853609a2f
gofmt <liangzi20190216@outlook.com>
2023-03-01 09:05:46 +0800

    dropdownbutton

--- example/lib/widgets/markdown.dart
+++ example/lib/widgets/markdown.dart
@@ -16,24 +16,52 @@ class _PageMartDownState extends State<PageMartDown> {
         appBar: AppBar(toolbarHeight: 30, centerTitle: true, title: Text("markdown+listView")),
         body: Column(
           children: [
-            SizedBox(
-                height: 60,
-                width: 300,
-                child: Expanded(
-                    child: DropdownButton(
-                  borderRadius: const BorderRadius.all(Radius.circular(6)),
-                  items: const [
-                    DropdownMenuItem(value: 0, child: Text("select list")),
-                    DropdownMenuItem(value: 1, child: Text("str1")),
-                    DropdownMenuItem(value: 2, child: Text("str2")),
-                    DropdownMenuItem(value: 3, child: Text("str3")),
-                  ],
-                  onChanged: (value) {
-                    setState(() {
-                      _str = indexMap[value]!;
-                    });
-                  },
-                ))),
+            Row(
+              children: [
+                Expanded(
+                    child: SizedBox(
+                        height: 60,
+                        width: 300,
+                        child: DropdownButton(
+                          hint: const Text('下拉选择1'),
+                          underline: const SizedBox(), //清除下划线
+                          borderRadius: const BorderRadius.all(Radius.circular(6)),
+                          items: const [
+                            DropdownMenuItem(value: 0, child: Text("select list")),
+                            DropdownMenuItem(value: 1, child: Text("str1")),
+                            DropdownMenuItem(value: 2, child: Text("str2")),
+                            DropdownMenuItem(value: 3, child: Text("str3")),
+                          ],
+                          onChanged: (value) {
+                            setState(() {
+                              _str = indexMap[value]!;
+                            });
+                          },
+                        ))),
+
+                ///
+                Expanded(
+                    child: SizedBox(
+                        height: 60,
+                        width: 300,
+                        child: DropdownButton(
+                          hint: const Text('下拉选择2'),
+                          underline: const SizedBox(),
+                          borderRadius: const BorderRadius.all(Radius.circular(6)),
+                          items: const [
+                            DropdownMenuItem(value: 0, child: Text("select list")),
+                            DropdownMenuItem(value: 1, child: Text("str1")),
+                            DropdownMenuItem(value: 2, child: Text("str2")),
+                            DropdownMenuItem(value: 3, child: Text("str3")),
+                          ],
+                          onChanged: (value) {
+                            setState(() {
+                              _str = indexMap[value]!;
+                            });
+                          },
+                        ))),
+              ],
+            ),
             Expanded(child: buildMarkdown())
           ],
         ));
